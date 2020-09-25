381eb05af9b8c8f2216bc37380acc4353640a1ad
Angel Samaniego <171G0634@rcarbonifera.tecnm.mx>
2020-09-25 16:27:04 -0500

    Subiendo archivos

---
+++ Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unidad_2Actividad2.Models
+{
+    public partial class Estados
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abrev { get; set; }
+    }
+}
